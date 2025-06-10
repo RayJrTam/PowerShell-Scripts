396191952d3421c4b9d42b2d489b82b77642e865
Raymond Tamse Jr <raymondstevejr@yahoo.com>
2025-06-11 08:39:44 +1200

    Planning to set up custom PIM that spans an entire week

--- pim-automation/Enable-SD-PIM.ps1
+++ pim-automation/Enable-SD-PIM.ps1
@@ -4,6 +4,15 @@
 
 cls
 
+# TO-DO: Create time schedule constants rather than asking for user input for specific times
+$option = Read-Host @"
+Select option:
+1. Enable PIM for today
+2. Enable PIM for custom date
+Input
+"@
+
+
 try { Import-Module AzureADPreview -ErrorAction Stop } catch { Install-Module -Name AzureADPreview -Scope CurrentUser }
 $User = Connect-AzureAD
 
@@ -19,11 +28,17 @@ $CurrentUser = Get-AzureADUser -Filter "UserPrincipalName eq '$UPN'"
 # Schedule
 $schedule = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedSchedule
 $schedule.Type = 'Once'
-$Start = (Get-Date).ToUniversalTime()
+
+if ($option -eq "1") {
+    $Start = (Get-Date).ToUniversalTime()
+} else {
+    $Start = Read-Host "Enter start date and time (e.g. 09/06/2025 08:00)"
+    $Start = $Start.ToUniversalTime()
+}
+
 $End = (Get-Date).AddHours(9).ToUniversalTime()
 $schedule.StartDateTime = $Start.ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
 $schedule.endDateTime = $End.ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
-
 
 Open-AzureADMSPrivilegedRoleAssignmentRequest -ProviderId aadGroups `
     -Schedule $schedule `
